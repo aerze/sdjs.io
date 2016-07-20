a5e34c9a9aac9969ca29e58d58172306ba7c90c1
Gus Suarez <gus.suarez@zeeto.io>
2016-07-19 20:57:54 -0700

    updates app to use refactored Routes, that pulls data from Loopback

--- app.js
+++ app.js
@@ -1,23 +1,31 @@
-var Router = require('./router')
-var table = require('./routes.json')
-var express = require('express')
+const router = require('./router');
+// const table = require('./routes.json');
+const express = require('express');
 
-var router = new Router(table)
-var app = express()
+const app = express();
 
-app.get(/.*/, function (req, res) {
-	var target = router.transform(req.path)
-	if (target) {
-		console.log("Request received: Redirecting %s to %s", req.path, target)
-		res.redirect(302, target)
-	} else {
-		console.error("Request received: Path not found! %s", req.path)
-		res.status(404).send('Sorry, I couldn\'t find that. :(');
-	}
-})
+router.init();
 
-//start server
-var server = app.listen(3000, function () {
-    var port = server.address().port
-    console.log('Server listening on port ' + port)
-})
+app.get('/reset', (req, res) => {
+  router.flushCheckCache();
+  res.sendStatus(200);
+});
+
+app.get(/.*/, (req, res) => {
+  router.transform(req.path, (target) => {
+    if (target) {
+      console.log('Request received: Redirecting %s to %s', req.path, target);
+      res.redirect(302, target);
+    } else {
+      console.error('Request received: Path not found! %s', req.path);
+      res.status(404).send('Sorry, I couldn\'t find that. :(');
+    }
+  });
+});
+
+
+// start server
+const server = app.listen(3001, () => {
+  const port = server.address().port;
+  console.log(`Server listening on port ${port}`);
+});
--- router.js
+++ router.js
@@ -1,9 +1,53 @@
-function Router (table) {
-	this.table = table
-}
+const request = require('request');
 
-Router.prototype.transform = function (route) {
-	return this.table[route]
-}
+const Router = {
+  jumplinks: [],
+  recentChecks: [],
 
-module.exports = Router
+  init(callback) {
+    const update = this.update.bind(this);
+    callback = callback || function noop() {};
+
+    request('http://localhost:3000/api/jumplinks', (error, response, body) => {
+      let jumplinks = [];
+      if (error) throw new Error(error);
+      if (response.statusCode === 200 && body) {
+        try {
+          jumplinks = JSON.parse(body);
+        } catch (parseError) {
+          throw new Error(parseError);
+        }
+
+        update(jumplinks);
+        callback();
+      }
+    });
+  },
+
+  transform(route, callback) {
+    const jumplink = this.jumplinks.filter((jumplinkItem) => (jumplinkItem.baseRoute === route));
+
+    // if we found any jumplinks, return the first one
+    if (jumplink.length > 0) return callback(jumplink[0].targetRoute);
+
+    // if we have recently check for it, return default
+    if (this.recentChecks.indexOf(route) >= 0) return callback(null);
+
+    // unknown route found, push to recent checks
+    this.recentChecks.push(route);
+
+    return this.init(() => {
+      this.transform(route, callback);
+    });
+  },
+
+  update(jumplinks) {
+    this.jumplinks = jumplinks;
+  },
+
+  flushCheckCache() {
+    this.recentChecks = [];
+  },
+};
+
+module.exports = Router;
